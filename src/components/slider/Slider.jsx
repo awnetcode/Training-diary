1eca013dc5a26f7f1f7780c259ec9e5bd590051f
awnetcode <155958835+awnetcode@users.noreply.github.com>
2025-02-18 11:08:38 +0100

    finish-slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,16 +1,33 @@
 import './slider.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import SliderCounter from './SliderCounter';
 
+import carlGustaf from './slider-images/Carl Gustaf.jpg';
+import ingram from './slider-images/ingram.jpg';
+import rossi from './slider-images/rossi Lever Action.jpg';
+import saa from './slider-images/SAA revolver.jpg';
+import sbr from './slider-images/SBR AR.avif';
+import uzi from './slider-images/uzi.jpg';
+
 const Slider = () => {
 
+    const images = [carlGustaf, ingram, rossi, saa, sbr, uzi]
     const [itemActive, setItemActive] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setItemActive(prev => (prev + 1) % images.length); // Zapętlenie indeksu
+        }, 5000);
+
+        return () => clearInterval(interval); // Czyszczenie interwału przy odmontowaniu
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
 
     return(
         <div id="slider-container">
-            <img src="" alt="" className="slider-image" />
+            <img src={images[itemActive]} alt="" className="slider-image" />
             <SliderCounter
             itemActive={itemActive}
             setItemActive={setItemActive}
--- src/components/slider/SliderCounter.jsx
+++ src/components/slider/SliderCounter.jsx
@@ -3,19 +3,19 @@ import './sliderCounter.css';
 // eslint-disable-next-line react/prop-types
 const SliderCounter = ({itemActive, setItemActive}) =>{
 
-    console.log(itemActive,setItemActive);
-    return(
-        <>
+    const totalItems = 6; // Liczba kropek (powinna odpowiadać liczbie obrazków)
+
+    return (
         <ul className="counter-list">
-            <li className="counter-item"></li>
-            <li className="counter-item"></li>
-            <li className="counter-item"></li>
-            <li className="counter-item"></li>
-            <li className="counter-item"></li>
-            <li className="counter-item"></li>
+            {[...Array(totalItems)].map((_, index) => (
+                <li 
+                    key={index} 
+                    className={`counter-item ${index === itemActive ? "counter-item-active" : ""}`}
+                    onClick={() => setItemActive(index)} // Możesz dodać kliknięcie do zmiany aktywnego slajdu
+                ></li>
+            ))}
         </ul>
-        </>
-    )
+    );
 }
 
 export default SliderCounter;
